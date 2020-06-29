cfe51b6972f3a9cd0e9c6d817ed56a2ca6e3940f
DamianYongjae <klick214@gmail.com>
2020-06-29 16:32:56 -0400

    changed url

--- src/Apollo/Client.ts
+++ src/Apollo/Client.ts
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 export default new ApolloClient({
-  uri: "https://reverent-johnson-8853fa.netlify.app/",
+  uri: "https://mailsender-api.vercel.app/",
   // process.env.NODE_ENV === "development"
   //   ? "http://localhost:4000"
   //   : "https://prismagram-backend-damian.herokuapp.com/",
