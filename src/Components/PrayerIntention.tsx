a031d90950ad88d0440b5e50b5d458fda810e6e1
DamianYongjae <klick214@gmail.com>
2020-06-29 20:41:52 -0400

    edit header

--- src/Components/PrayerIntention.tsx
+++ src/Components/PrayerIntention.tsx
@@ -123,12 +123,9 @@ export default () => {
     } else {
       await fetch(`https://mailsender-api.vercel.app/sendmail`, {
         body: JSON.stringify({ email, intention }),
-        // headers: {
-        //   "Content-Type": "application/json",
-        //   "Access-Control-Allow-Origin": "*",
-        //   "Access-Control-Allow-Headers":
-        //     "Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Origin",
-        // },
+        headers: {
+          "Content-Type": "application/json",
+        },
         mode: "cors",
       });
       history.push("/complete");
