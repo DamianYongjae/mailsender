136f425b98ac481b888c4bcd0759a215300cec73
DamianYongjae <klick214@gmail.com>
2020-06-29 20:34:24 -0400

    remove credential

--- src/Components/PrayerIntention.tsx
+++ src/Components/PrayerIntention.tsx
@@ -129,7 +129,6 @@ export default () => {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Headers":
             "Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Origin",
-          "Access-Control-Allow-Credentials": "true",
         },
         mode: "cors",
       });
