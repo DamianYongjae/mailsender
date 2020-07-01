fcaf2cd05b5e63287d783eb9a239d7cae4bc7d6a
DamianYongjae <klick214@gmail.com>
2020-07-01 17:43:30 -0400

    headers

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -1,17 +1,16 @@
 export const fetchPost = async ({ endpoint, data }: any) => {
-  return fetch(`https://mailsender-api.vercel.app/${endpoint}`, {
+  return fetch(`https://https://mailsender-api.vercel.app/${endpoint}`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "*",
+      "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
-      "Access-Control-Allow-Credentials": "true",
+      //   "Access-Control-Allow-Headers":
+      //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
+      //   "Access-Control-Allow-Credentials": "true",
     },
     mode: "cors",
+  }).then((res) => {
+    return res;
   });
-  //   .then((res) => {
-  //     return res;
-  //   });
 };
