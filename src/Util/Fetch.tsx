eee2bff6601691796bf45a038fbf22f2adbd8103
DamianYongjae <klick214@gmail.com>
2020-07-01 17:50:19 -0400

    accept

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -5,6 +5,7 @@ export const fetchPost = async ({ endpoint, data }: any) => {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
       //   "Access-Control-Allow-Headers":
       //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
       //   "Access-Control-Allow-Credentials": "true",
