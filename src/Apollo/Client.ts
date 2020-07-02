fbdfe6598ea6ba2c0a7b8f68c9ddeacaf4b961b7
DamianYongjae <klick214@gmail.com>
2020-07-02 10:03:58 -0400

    rollback

--- src/Apollo/Client.ts
+++ src/Apollo/Client.ts
@@ -1,29 +1,11 @@
-import {ApolloClient,HttpLink} from "apollo-boost";
+import ApolloClient from "apollo-boost";
 
-export default () => {
-  return new ApolloClient({
-    // uri: "https://mailsender-api.vercel.app/",
-    // // process.env.NODE_ENV === "development"
-    // //   ? "http://localhost:4000"
-    // //   : "https://prismagram-backend-damian.herokuapp.com/",
-    // headers: {
-    //   mode: "cors",
-    // },
-    link: new HttpLink({
-      uri: `https://mailsender-api.vercel.app/`, // Server URL (must be absolute)
-      credentials: "same-origin", // Additional fetch() options like `credentials` or `headers`
-      // Use fetch() polyfill on the server
-      fetch: (fetch as any),
-      // headers: {
-      //   Authorization:
-      //     initialState && `Bearer ${initialState.cookies[cookieName.token]}`,
-      // },
-      fetchOptions: {
-        mode: "cors",
-      },
-      
-  }),
-  cache: new InMemoryCache({
-    dataIdFromObject: (o) => o.id,
-  }).restore(initialState),
-}
+export default new ApolloClient({
+  uri: "https://mailsender-api.vercel.app/",
+  // process.env.NODE_ENV === "development"
+  //   ? "http://localhost:4000"
+  //   : "https://prismagram-backend-damian.herokuapp.com/",
+  headers: {
+    mode: "cors",
+  },
+});
