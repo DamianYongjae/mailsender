fd4af147cc20421176fdba7700837ee941cc65d0
DamianYongjae <klick214@gmail.com>
2020-07-02 13:48:15 -0400

    url

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -1,17 +1,20 @@
 export const fetchPost = async ({ endpoint, data }: any) => {
-  return fetch(`https://mailsender-api.vercel.app/${endpoint}`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      Accept: "application/json",
-      //   "Access-Control-Allow-Headers":
-      //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
-      //   "Access-Control-Allow-Credentials": "true",
-    },
-    mode: "cors",
-  }).then((res) => {
+  return fetch(
+    `https://mailsender-delta.vercel.app/#/https://mailsender-api.vercel.app/${endpoint}`,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        //   "Access-Control-Allow-Headers":
+        //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
+        //   "Access-Control-Allow-Credentials": "true",
+      },
+      mode: "cors",
+    }
+  ).then((res) => {
     return res;
   });
 };
