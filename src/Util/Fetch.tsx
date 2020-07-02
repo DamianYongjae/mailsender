1d7a626cf97239fefbec4fde5e7c10351b2d6cc0
DamianYongjae <klick214@gmail.com>
2020-07-02 13:53:35 -0400

    change url

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -1,20 +1,17 @@
 export const fetchPost = async ({ endpoint, data }: any) => {
-  return fetch(
-    `https://mailsender-delta.vercel.app/#/prayerintention/https://mailsender-api.vercel.app/${endpoint}`,
-    {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        Accept: "application/json",
-        //   "Access-Control-Allow-Headers":
-        //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
-        //   "Access-Control-Allow-Credentials": "true",
-      },
-      mode: "cors",
-    }
-  ).then((res) => {
+  return fetch(`https://mailsender-api.vercel.app/${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Accept: "application/json",
+      //   "Access-Control-Allow-Headers":
+      //     "X-Requested-With, Access-Control-Allow-Origin, X-HTTP-Method-Override, Content-Type, Authentication, Accept",
+      //   "Access-Control-Allow-Credentials": "true",
+    },
+    mode: "cors",
+  }).then((res) => {
     return res;
   });
 };
