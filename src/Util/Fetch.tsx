8a8ee00910930fb5b63abad2b54b525fb32464a9
DamianYongjae <klick214@gmail.com>
2020-07-03 17:06:27 -0400

    change url

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -1,5 +1,5 @@
 export const fetchPost = async ({ endpoint, data }: any) => {
-  return fetch(`http://localhost:4000/${endpoint}`, {
+  return fetch(`https://mailsender-api.vercel.app/${endpoint}`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
