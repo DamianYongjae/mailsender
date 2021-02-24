a775cb332b47189e8c863c98d2b7057805cffc76
DamianYongjae <klick214@gmail.com>
2021-02-24 13:34:22 -0500

    port

--- src/Util/Fetch.tsx
+++ src/Util/Fetch.tsx
@@ -1,11 +1,11 @@
 export const fetchPost = async ({ endpoint, data }: any) => {
   // return fetch(`https://mailsender-api-john.herokuapp.com/${endpoint}`, {
-  return fetch(`https://mailsender-api.vercel.app/${endpoint}`, {
+  return fetch(`https://mailsender-api.vercel.app:4000/${endpoint}`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
       "Content-Type": "application/json",
     },
-    mode: "no-cors",
+    mode: "cors",
   });
 };
